90ac53670a1624d2f5b3d2825dbceb973569ca8b
Rex Banny <abbaslielmir123@gmail.com>
2026-04-10 12:09:59 +0400

    first commit

---
+++ signup/signup.js
@@ -0,0 +1,45 @@
+const sbt_button = document.getElementById("sbt-button");
+
+function emailTest() {}
+
+function cfn() {
+  const error = document.getElementById("error");
+  let password = document.getElementById("password").value;
+  let password_cfn = document.getElementById("password-cfn").value;
+  error.textContent = "";
+
+  const emailerror = document.getElementById("error");
+  let email = document.getElementById("email").value;
+  emailerror.textContent = "";
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (email.trim() === "") {
+    emailerror.textContent = "email can't be empty";
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    emailerror.textContent = "email format is wronge";
+    return;
+  }
+
+  if (password.length < 6) {
+    error.textContent = "Password must be more than 6 character";
+    return;
+  }
+  if (password !== password_cfn) {
+    error.textContent = "Password must be same as other password";
+    return;
+  }
+  if (!/[A-Z]/.test(password)) {
+    error.textContent = "Password include least one alfabeth";
+    return;
+  }
+  if (!/[0-9]/.test(password)) {
+    error.textContent = "Password include least one number";
+    return;
+  }
+}
+
+sbt_button.addEventListener("click", cfn);
